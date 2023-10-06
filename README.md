a6dbc052192658433c7fc8b1acc4739525589afa
arnaudwilczynski2023 <arnaud.wilczynski@insee.fr>
2023-10-06 22:15:20 +0000

    rajout gouvdown et fin prepa exemple

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # template_quarto_with_parameters
+
+Consignes pour l'application:  
+créer un document quarto via file/new file (décocher l'éditeur visuel)  
+Compiler et créer le html avec render  
+Modifier les options des chunks et vérifier ce qui s'affiche en sortie cf [guide](https://quarto.org/docs/computations/execution-options.html)  
+Regarder penguins.qmd pour un rapport paramétré
--- lancement_html.R
+++ lancement_html.R
@@ -1,9 +1,10 @@
-year_param <- "2021"
+year_param <- "2008"
 sex_param <- "female"
 quarto::quarto_render(input = "penguins.qmd",
                       #output_file = paste0("rapports/penguins_",sex_param,".html"),
-                      execute_params = list(#"year_select" = 2021,
-                                            "sex_select" = "female"))
+                      execute_params = list("year_select" = 2008#,
+                                            #"sex_select" = "female"
+                                            ))
 # et vous pouvez boucler sur les multiples combinaisons avec une boucle ou plus 
 # joli avec purrr
 
@@ -11,3 +12,8 @@ quarto::quarto_render(input = "penguins.qmd",
 # c'est encore en développement car on aimerait ranger le html dans un dossier
 # rapports pour organiser correctement son projet
 # là il faudrait le déplacer à la main avec un file.copy
+
+# la solution serait de lancer en ligne de commande
+# quarto render template_quarto_with_parameters/penguins.qmd -P year_select:2008 -P sex_select:"female"
+# donc pour produire tous les rapports sur les années et les sexes
+# faire un batch et une boucle... pas vraiment ce que l'on a avec rmd pour le moment
