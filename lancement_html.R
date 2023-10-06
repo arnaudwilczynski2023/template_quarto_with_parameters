90e94e20693817b6938961b4f09c327cde99fc27
arnaudwilczynski2023 <arnaud.wilczynski@insee.fr>
2023-10-06 20:30:41 +0000

    je repasse avec un seul parametre pour que le render externe marche

--- lancement_html.R
+++ lancement_html.R
@@ -1,11 +1,13 @@
 year_param <- "2021"
 sex_param <- "female"
 quarto::quarto_render(input = "penguins.qmd",
-                      execute_params = list("year_select" = 2021,
+                      #output_file = paste0("rapports/penguins_",sex_param,".html"),
+                      execute_params = list(#"year_select" = 2021,
                                             "sex_select" = "female"))
 # et vous pouvez boucler sur les multiples combinaisons avec une boucle ou plus 
 # joli avec purrr
 
-# bon ça marche très bien sur rmd mais en multiparamètres de nombreuses issues
-# semblent encore non résolues, notamment pour ranger la sortie dans un
-# dossier rapports
+# bon ça marche pas très bien qmd en multiparamètres ou pour indiquer un chemin de sortie
+# c'est encore en développement car on aimerait ranger le html dans un dossier
+# rapports pour organiser correctement son projet
+# là il faudrait le déplacer à la main avec un file.copy
